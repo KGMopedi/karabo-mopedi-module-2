c1147737d4eba96400a12fc6f25e01f4d0c78cf9
KGMopedi <61540084+KGMopedi@users.noreply.github.com>
2022-06-21 03:16:23 +0200

    Add files via upload

---
+++ MyFirstDart.dart
@@ -0,0 +1,9 @@
+void main() {
+ String name = "Karabo";
+ String favoriteApp = "TikTok";
+ String city = "Mafikeng";
+
+  print("Name is $name");
+  print("Favorite App is $favoriteApp");
+  print("City is $city");
+}
