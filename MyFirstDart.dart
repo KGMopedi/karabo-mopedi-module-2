cf6c0366a6cb073f5bfbe4708b249038ea39a3f3
KGMopedi <61540084+KGMopedi@users.noreply.github.com>
2022-06-21 03:26:36 +0200

    Delete MyFirstDart.dart

--- MyFirstDart.dart
+++
@@ -1,9 +0,0 @@
-void main() {
- String name = "Karabo";
- String favoriteApp = "TikTok";
- String city = "Mafikeng";
-
-  print("Name is $name");
-  print("Favorite App is $favoriteApp");
-  print("City is $city");
-}
